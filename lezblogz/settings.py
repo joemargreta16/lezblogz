f144243d2196f4b1b0c3d44808051709dd2176c0
Greta <joemar.greta16@gmail.com>
2021-03-14 16:56:16 +0800

    update again 03/14/2021

--- lezblogz/settings.py
+++ lezblogz/settings.py
@@ -189,20 +189,20 @@ ACCOUNT_EMAIL_UNIQUE = True
 
 
 # comment lang muna kapag i deploy
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join( BASE_DIR, "sent_mails" )
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = os.path.join( BASE_DIR, "sent_mails" )
 
 # comment lang muna kapag i localhost
 # smtp
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIl_PORT = 587
-# EMAIL_HOST_USER = 'joemar.greta16@gmail.com'
-# EMAIL_HOST_PASSWORD = 'mhvkaoomuyaoltcp'
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'lezblogz <joemar.greta16@gmail.com>'
-#
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIl_PORT = 587
+EMAIL_HOST_USER = 'joemar.greta16@gmail.com'
+EMAIL_HOST_PASSWORD = 'mhvkaoomuyaoltcp'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'lezblogz <joemar.greta16@gmail.com>'
+
 #   for deployment
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
